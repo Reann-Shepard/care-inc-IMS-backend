5c0aea48eb63ff397c9672d69a3a2c0b7f9d3900
HarryLee87 <haewon.harry.lee@gmail.com>
2024-06-03 05:39:09 -0600

    Revert "added comments values as null in package seed file"
    This reverts commit 022c4d13029aa93a1a14cd7dfb45a5f81e9974eb.

--- prisma/seed/package.ts
+++ prisma/seed/package.ts
@@ -8,35 +8,30 @@ export async function seedPackages() {
       fittingDate: new Date('2024-01-01'),
       warrantyExpiration: new Date('2025-01-01'),
       orderCustomerId: 1,
-      comments: null,
     },
     {
       clientId: 2,
       fittingDate: new Date('2024-02-01'),
       warrantyExpiration: new Date('2025-02-01'),
       orderCustomerId: 2,
-      comments: null,
     },
     {
       clientId: 3,
       fittingDate: new Date('2024-03-01'),
       warrantyExpiration: new Date('2025-03-01'),
       orderCustomerId: 3,
-      comments: null,
     },
     {
       clientId: 4,
       fittingDate: new Date('2024-04-01'),
       warrantyExpiration: new Date('2025-04-01'),
       orderCustomerId: 4,
-      comments: null,
     },
     {
       clientId: 5,
       fittingDate: new Date('2024-05-01'),
       warrantyExpiration: new Date('2025-05-01'),
       orderCustomerId: 5,
-      comments: null,
     },
   ];
 
