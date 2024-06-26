31133348345d293902868af04af8b374c9345c41
Laurenchansj <laurenchansj@gmail.com>
2024-06-26 00:54:31 -0600

    Filter function in Pacakge

--- src/modules/package/package.controller.ts
+++ src/modules/package/package.controller.ts
@@ -6,13 +6,45 @@ export class PackageController {
   constructor(private readonly packageService: PackageService) {}
 
   @Get()
-  getAllPackages(@Query('sortBy') sortBy?: string) {
+  getAllPackages(@Query() query: { [key: string]: string | string[] }) {
+    const { sortBy, ...filterBy } = query;
+    const filter = {};
+    // let filter: { [key: string]: any[] } = {};
+
+    // if (filterBy.packageId || filterBy.clientId) {
+    //   const filterCondition = [];
+
+    //   if (filterBy.packageId) {
+    //     filter['id'] = Array.isArray(filterBy.packageId)
+    //       ? filterBy.packageId.map((id) => +id)
+    //       : [+filterBy.packageId];
+    //     filterCondition.push({ id: { in: filter['id'] } });
+    //   }
+    //   if (filterBy.clientId) {
+    //     filter['clientId'] = Array.isArray(filterBy.clientId)
+    //       ? filterBy.clientId.map((id) => +id)
+    //       : [+filterBy.clientId];
+    //     filterCondition.push({ clientId: { in: filter['clientId'] } });
+    //   }
+    //   filter = {
+    //     OR: filterCondition,
+    //   };
+    // }
+
+    if (filterBy.packageId) {
+      filter['id'] = Array.isArray(filterBy.packageId)
+        ? filterBy.packageId.map((id) => +id)
+        : [+filterBy.packageId];
+    }
+    if (filterBy.clientId) {
+      filter['clientId'] = Array.isArray(filterBy.clientId)
+        ? filterBy.clientId.map((id) => +id)
+        : [+filterBy.clientId];
+    }
+
     try {
-      if (sortBy) {
-        return this.packageService.getPackageSortBy(sortBy);
-      } else {
-        return this.packageService.getAllPackages();
-      }
+      const sortCon = Array.isArray(sortBy) ? sortBy[0] : sortBy || 'id';
+      return this.packageService.getAllPackages(sortCon, filter);
     } catch (error) {
       console.log(error);
     }
--- src/modules/package/package.service.ts
+++ src/modules/package/package.service.ts
@@ -5,12 +5,18 @@ import { PrismaService } from '../prisma/prisma.service';
 export class PackageService {
   constructor(private prisma: PrismaService) {}
 
-  async getAllPackages() {
-    return this.prisma.package.findMany();
-  }
-
-  async getPackageSortBy(sortBy: string) {
+  async getAllPackages(
+    sortBy: string,
+    filterBy: { [key: string]: any[] } = {},
+  ) {
+    const filterQuery = Object.keys(filterBy).reduce((acc, key) => {
+      if (filterBy[key].length > 0) {
+        acc[key] = { in: filterBy[key] };
+      }
+      return acc;
+    }, {} as any);
     return this.prisma.package.findMany({
+      where: filterQuery,
       orderBy: {
         [sortBy]: 'asc',
       },
