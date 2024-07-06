fd3cb205e591ba7f4918a4a66fa2d55d335fa8b7
HarryLee87 <haewon.harry.lee@gmail.com>
2024-07-06 17:24:01 -0600

    fixed seed files

--- prisma/seed/orderManufacturer.ts
+++ prisma/seed/orderManufacturer.ts
@@ -4,37 +4,22 @@ const prisma = new PrismaClient();
 export async function seedOrderManufacturers() {
   const orderManufacturers = [
     {
-      manufacturerId: 1,
-      colorId: 1,
-      typeId: 1,
       orderDate: new Date('2024-01-01'),
       amount: 100,
     },
     {
-      manufacturerId: 2,
-      colorId: 2,
-      typeId: 2,
       orderDate: new Date('2024-02-01'),
       amount: 55,
     },
     {
-      manufacturerId: 3,
-      colorId: 3,
-      typeId: 3,
       orderDate: new Date('2024-03-01'),
       amount: 60,
     },
     {
-      manufacturerId: 4,
-      colorId: 4,
-      typeId: 4,
       orderDate: new Date('2024-04-01'),
       amount: 65,
     },
     {
-      manufacturerId: 5,
-      colorId: 5,
-      typeId: 5,
       orderDate: new Date('2024-05-01'),
       amount: 80,
     },
@@ -43,7 +28,6 @@ export async function seedOrderManufacturers() {
   for (const orderManufacturer of orderManufacturers) {
     const existingOrderManufacturer = await prisma.orderManufacturer.findFirst({
       where: {
-        manufacturerId: orderManufacturer.manufacturerId,
         orderDate: orderManufacturer.orderDate,
       },
     });
@@ -54,14 +38,10 @@ export async function seedOrderManufacturers() {
       });
       console.log(`OrderManufacturer Created: ${newOrderManufacturer.id}`);
     } else {
-      console.log(
-        `OrderManufacturer already exists for manufacturerId: ${orderManufacturer.manufacturerId} and orderDate: ${orderManufacturer.orderDate}`,
-      );
+      console.log(`orderDate: ${orderManufacturer.orderDate}`);
       await prisma.orderManufacturer.update({
         where: { id: existingOrderManufacturer.id },
         data: {
-          colorId: orderManufacturer.colorId,
-          typeId: orderManufacturer.typeId,
           amount: orderManufacturer.amount,
         },
       });
