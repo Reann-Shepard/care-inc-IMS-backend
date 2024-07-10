3cab2341b099ba808f35a97c3e1621f74630014e
HarryLee87 <haewon.harry.lee@gmail.com>
2024-07-10 06:20:52 -0600

    removed comments

--- src/modules/order-manufacturer/order-manufacturer.service.ts
+++ src/modules/order-manufacturer/order-manufacturer.service.ts
@@ -59,45 +59,6 @@ export class OrderManufacturerService {
     });
   }
 
-  // async updateOrderManufacturerById(
-  //   id: number,
-  //   updateOrderManufacturerDto: UpdateOrderManufacturerDto,
-  // ) {
-  //   const { OrderDevices = [], ...orderManufacturerData } =
-  //     updateOrderManufacturerDto;
-
-  //   try {
-  //     await this.prisma.orderManufacturer.update({
-  //       where: { id: Number(id) },
-  //       data: orderManufacturerData,
-  //     });
-
-  //     for (const orderDevice of OrderDevices) {
-  //       await this.prisma.orderDevice.update({
-  //         where: {
-  //           deviceId_orderManufacturerId: {
-  //             deviceId: Number(orderDevice.deviceId),
-  //             orderManufacturerId: Number(id),
-  //           },
-  //         },
-  //         data: {
-  //           device: {
-  //             update: {
-  //               ...orderDevice.device,
-  //             },
-  //           },
-  //         },
-  //       });
-  //     }
-
-  //     return this.getOrderManufacturerById(Number(id));
-  //   } catch (error) {
-  //     console.error('Error updating order manufacturer:', error);
-  //     throw new InternalServerErrorException(
-  //       'Failed to update order manufacturer',
-  //     );
-  //   }
-  // }
   async updateOrderManufacturerById(
     id: number,
     updateOrderManufacturerDto: UpdateOrderManufacturerDto,
