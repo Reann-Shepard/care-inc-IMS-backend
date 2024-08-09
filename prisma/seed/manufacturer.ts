18130ca90ca437fd1190ae4be687d14b8a47c398
HarryLee87 <haewon.harry.lee@gmail.com>
2024-08-08 18:12:15 -0600

    fixed env bugs

--- prisma/seed/manufacturer.ts
+++ prisma/seed/manufacturer.ts
@@ -5,7 +5,7 @@ export async function seedManufacturers() {
   const manufacturers = [
     { id: 1, name: 'Unitron' },
     { id: 2, name: 'Unitron S' },
-    { id: 3, name: 'Signia C&G' },
+    { id: 3, name: 'Signia CG' },
     { id: 4, name: 'Signia Silk' },
     { id: 5, name: 'Oticon' },
   ];
